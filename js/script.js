185a3a98f97faa2edfadd81731a1425e9787ddfe
kizuyoko <kizuyoko@gmail.com>
2024-02-06 15:35:07 +0100

    Some Adjustments

--- js/script.js
+++ js/script.js
@@ -28,21 +28,16 @@ function onTabClick(e) {
 }
 
 // Hamburger menu for mobile
-const btn = document.getElementById('menu-btn')
+const menuBtn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 const logo = document.getElementById('logo')
 
 // Hamburger button listener
-btn.addEventListener('click', navToggle)
+menuBtn.addEventListener('click', navToggle)
 
 function navToggle() {
-  btn.classList.toggle('open')
+  menuBtn.classList.toggle('open')
   menu.classList.toggle('flex')
   menu.classList.toggle('hidden')
-
-  if (menu.classList.contains('flex')) {
-    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
-  } else {
-    logo.setAttribute('src', './images/logo-bookmark.svg')
-  }
+  logo.classList.toggle('text-white')
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
       xl: '1440px',
     },
     extend: {
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
       fontFamily: {
         sans: ['Quicksand', 'sans-serif'],
       },
